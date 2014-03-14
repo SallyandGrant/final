c9f11792499366ee21cd9192407dd6d95f51ab24
gyunker <gyunker2014@kellogg.northwestern.edu>
2014-03-14 17:02:14 -0500

    ConfirmationEmail

--- app/mailers/confirmation.rb
+++ app/mailers/confirmation.rb
@@ -2,7 +2,7 @@ class Confirmation < ActionMailer::Base
   default from: "noreply@pintxopop.com"
 
   def reservation_created(reservation)
-    mail(to: gyunker2014@kellogg.northwestern.edu,
+    mail(to: "ssimpson2014@kellogg.northwestern.edu",
       subject: 'Reservation was Made')
 
 
